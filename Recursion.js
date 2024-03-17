e87240cdb223b2648fdd4f842c64c930de05e36d
Arulmurugan Kanagasabapathi <arulmurugan194@gmail.com>
2024-03-17 07:57:58 +0530

    Update Recursion

--- Recursion.js
+++ Recursion.js
@@ -1,1 +1,22 @@
+// Print number from N to 1
+function printNto1(num){
+  if(num === 0){
+    return
+  }
+	console.log(num)
+  printNto1(num-1)
+}
 
+printNto1(5)
+
+// Print number from 1 to N
+function print1ton(num){
+  if(num === 0){
+    return
+  }
+	
+  print1ton(num-1)
+  console.log(num)
+}
+
+print1ton(5)
